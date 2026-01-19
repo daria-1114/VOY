32caf33ecab9cc525e5b2d51c51bb4bef7cc7932
daria-1114 <daryatoma4111@gmail.com>
2026-01-19 18:30:49 +0200

    added Firebase Authentication

--- app/src/main/java/com/example/voy/activities/SignupTabFragment.java
+++ app/src/main/java/com/example/voy/activities/SignupTabFragment.java
@@ -11,8 +11,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.voy.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
 
 public class SignupTabFragment extends Fragment {
 
@@ -26,11 +31,37 @@ public class SignupTabFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        EditText emailEt = getView().findViewById(R.id.signUp_email);
+        EditText passwordEt = getView().findViewById(R.id.signup_password);
+        EditText confirmPasswordEt = getView().findViewById(R.id.signup_confirm);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
         Button signupBtn = view.findViewById(R.id.signup_btn);
-        Intent intent = new Intent(getActivity(),MainActivity.class);
+
         signupBtn.setOnClickListener(v->{
-            startActivity(intent);
-            requireActivity().finish();
+            String email = emailEt.getText().toString().trim();
+            String password = passwordEt.getText().toString().trim();
+            String confirmPassword = confirmPasswordEt.getText().toString().trim();
+            if(email.isEmpty()||password.isEmpty()||confirmPassword.isEmpty()){
+                Toast.makeText(getContext(),"Fill all fields!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if(!password.equals(confirmPassword)){
+                Toast.makeText(getContext(),"Passwords must match!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            auth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(task -> {
+                        if(task.isSuccessful()){
+                            startActivity(new Intent(getContext(),MainActivity.class));
+                            requireActivity().finish();
+                        }else{
+                            Toast.makeText(getContext(), Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    });
         });
     }
 }
