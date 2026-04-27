57dddf0c91bfe16dcae5bbaffbdd8b1473045e62
daria-1114 <daryatoma4111@gmail.com>
2026-04-27 20:20:00 +0300

    added infoActivity and deleted MapsActivity

--- app/src/main/java/com/example/voy/activities/InfoActivity.java
+++ app/src/main/java/com/example/voy/activities/InfoActivity.java
@@ -1,4 +1,23 @@
 package com.example.voy.activities;
 
-public class InfoActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.voy.R;
+import com.google.android.material.appbar.MaterialToolbar;
+
+public class InfoActivity extends AppCompatActivity {
+    MaterialToolbar toolbar;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+        toolbar = findViewById(R.id.infoToolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(v->{
+            finish();
+        });
+    }
 }
